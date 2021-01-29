d9c4b839f079f6e5e82b9e0df02f6d599f1208b3
Isaac Batista <isaacbatst@gmail.com>
2021-01-29 16:31:19 -0300

    implementa setup de eventos

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+const handleSearchEvent = () => {
+  const searchInput = document.querySelector('#currency-input');
+  const currency = searchInput.value.toUpperCase();
+}
+
 const setupEvents = () => {
-  
+  const searchButton = document.querySelector('#search-button');
+  searchButton.addEventListener('click', handleSearchEvent);
 }
 
 window.onload = () => {
